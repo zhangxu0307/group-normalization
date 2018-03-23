81c0725520e4e24884109d8b919ca3ea2f0fa250
zhangxu0307 <zhangxu0307@163.com>
2018-03-23 18:00:48 +0800

    add Lenet-5 toy test

--- group_normalization.py
+++ group_normalization.py
@@ -5,33 +5,31 @@ import torch.functional as F
 
 class GroupNormalization(nn.Module):
 
-    def __init__(self, G, train = False, eps=1e-5):
+    def __init__(self, channelNum, G,  eps=1e-5):
 
         super().__init__()
-        self.gamma = nn.Parameter(th.randn(1))
-        self.beta = nn.Parameter(th.randn(1))
+        self.channelNum = channelNum
+        self.gamma = nn.Parameter(th.randn(self.channelNum, 1, 1))
+        self.beta = nn.Parameter(th.randn(self.channelNum, 1, 1))
         self.G = G
         self.eps = eps
-        self.mean = 0
-        self.var = 0
-        self.train = train
+
 
     def forward(self, x):
 
-        if self.train == True:
+            N, C, H, W = x.size()
 
-            N, C, H, W = x.size().data
-            x = x.view(N, self.G, C//self.G, H, W)
+            x = x.view(N, self.G, -1)
 
-            mean = th.mean(x, dim=[2, 3, 4], keepdim=True)
-            var = th.var(x, dim=[2, 3, 4], keepdim=True)
+            mean = x.mean(dim=2, keepdim=True)
+            var = x.std(dim=2, keepdim=True)
 
             x = (x-mean) / th.sqrt(var+self.eps)
 
             x = x.view(N, C, H, W)
 
-            return self.gamma * x + self.beta
+            out = self.gamma * x + self.beta
 
-        if self.train == False:
-            pass
+            return out
+
 
---
+++ train.py
@@ -0,0 +1,82 @@
+import torch as th
+import torchvision
+from torch.autograd import Variable
+from torch import nn
+from torch import optim
+from torchvision import datasets
+import torchvision.transforms as transforms
+from model import *
+
+
+def loadMNIST(batchSize):
+
+    root = "./data/"
+
+    trans= transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+
+    train_set = datasets.MNIST(root=root, train=True, transform=trans, download=True)
+    test_set = datasets.MNIST(root=root, train=False, transform=trans)
+
+    train_loader = th.utils.data.DataLoader(dataset=train_set, batch_size=batchSize, shuffle=True)
+    test_loader = th.utils.data.DataLoader(dataset=test_set, batch_size=batchSize, shuffle=False)
+
+    print ('==>>> total trainning batch number: {}'.format(len(train_loader)))
+    print ('==>>> total testing batch number: {}'.format(len(test_loader)))
+
+    return train_loader, test_loader
+
+
+def train(model, batchSize, epoch, useCuda = False):
+
+    if useCuda:
+        model = model.cuda()
+
+    optimizer = optim.Adam(model.parameters(), lr=0.1)
+    ceriation = nn.CrossEntropyLoss()
+    trainLoader, testLoader = loadMNIST(batchSize=batchSize)
+
+    for i in range(epoch):
+
+        # trainning
+        sum_loss = 0
+
+        for batch_idx, (x, target) in enumerate(trainLoader):
+            optimizer.zero_grad()
+            if useCuda:
+                x, target = x.cuda(), target.cuda()
+            x, target = Variable(x), Variable(target)
+            out = model(x)
+
+            loss = ceriation(out, target)
+            sum_loss += loss.data[0]
+            loss.backward()
+            optimizer.step()
+
+            if (batch_idx + 1) % 10 == 0 or (batch_idx + 1) == len(trainLoader):
+                print('==>>> epoch: {}, batch index: {}, train loss: {:.6f}'.format( i, batch_idx + 1, sum_loss/batch_idx))
+
+        # testing
+        correct_cnt, sum_loss = 0, 0
+        total_cnt = 0
+        for batch_idx, (x, target) in enumerate(testLoader):
+            if useCuda:
+                x, targe = x.cuda(), target.cuda()
+            x, target = Variable(x, volatile=True), Variable(target, volatile=True)
+            out = model(x)
+            loss = ceriation(out, target)
+            _, pred_label = th.max(out.data, 1)
+            total_cnt += x.data.size()[0]
+            correct_cnt += (pred_label == target.data).sum()
+
+            # smooth average
+            if (batch_idx + 1) % 100 == 0 or (batch_idx + 1) == len(testLoader):
+                print('==>>> epoch: {}, batch index: {}, test loss: {:.6f}, acc: {:.3f}'.format(
+                    i, batch_idx + 1, sum_loss/batch_idx, correct_cnt * 1.0 / total_cnt))
+
+    th.save(model.state_dict(), model.name())
+
+if __name__ == '__main__':
+
+    model = LeNet()
+    train(model, 128, 10)
+
